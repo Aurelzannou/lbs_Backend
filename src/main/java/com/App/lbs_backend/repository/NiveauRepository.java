3d605b031cb2bc1922d8549618c843563f8c274c
loick zannou <aurelzannou9@gmail.com>
2026-04-01 08:48:13 +0100

    recherche depuis le backend

--- src/main/java/com/App/lbs_backend/repository/NiveauRepository.java
+++ src/main/java/com/App/lbs_backend/repository/NiveauRepository.java
@@ -1,8 +1,17 @@
 package com.App.lbs_backend.repository;
 
 import com.App.lbs_backend.entity.Niveau;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NiveauRepository extends BaseRepository<Niveau> {
+
+    @Override
+    @Query("SELECT n FROM Niveau n WHERE lower(n.code) LIKE lower(concat('%', :filter, '%')) OR lower(n.libelle) LIKE lower(concat('%', :filter, '%'))")
+    Page<Niveau> findByLabelContaining(@Param("filter") String filter, Pageable pageable);
 }
+
